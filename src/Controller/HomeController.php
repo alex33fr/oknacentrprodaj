faa0e79f1a99093dadb4094ea26353e8f2bd4be5
aprivalov <a.privalov33@gmail.com>
2020-02-03 21:04:32 +0100

    Add Product fixtures on home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -10,11 +13,29 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param ProductRepository $repo
+     * @return Response
      */
-    public function index()
+    public function index(ProductRepository $repo)
     {
+        $products = $repo->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'products' => $products
+        ]);
+    }
+
+
+    /**
+     * @Route("/product/{id}", name="show_product")
+     * @param Product $product
+     * @return Response
+     */
+    public function show(Product $product){
+
+        return $this->render('home/product.html.twig',[
+                'product' => $product
         ]);
     }
 }
