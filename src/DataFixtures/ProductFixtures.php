fabe84aa3a424f9ea831c1e9d7e70b49e22885f1
aprivalov <a.privalov33@gmail.com>
2020-02-03 21:02:16 +0100

    Install and create Product fixture, added product on first page, edited menu on base.html

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -4,13 +4,30 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Product;
 
 class ProductFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 10; $i++){
+
+        $product = new Product();
+        $product->setProductColor("Red")
+                ->setProductName("Produit n°$i")
+                ->setProductImage("http://placehold.it/350x150")
+                ->setProductDescriptionOne("Description de prduit 1:")
+                ->setProductDescriptionTwo("Description product 2")
+                ->setProductCode("Dejefhe5125")
+                ->setProductQuantity("5")
+                ->setProductSwitchPrice(1)
+                ->setProductLowPrice(500)
+                ->setProductPrice(1000)
+                ->setProductCoupon(50)
+                ->setProductCreatedAt(new \DateTime());
+
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
